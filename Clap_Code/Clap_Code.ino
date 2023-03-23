5e90580388351386cd60f0364b6bc78fbd7eb747
Romiya Dangol <91256132+romiyadangol@users.noreply.github.com>
2023-03-23 19:23:55 +0545

    initial commit

---
+++ Clap_Code/Clap_Code.ino
@@ -0,0 +1,23 @@
+const int SENSOR_PIN = 2;
+const int Light_PIN = 12;
+ 
+void setup ()  
+{   
+  pinMode(SENSOR_PIN, INPUT); // Set the Sensor pin as INPUT  
+  pinMode(Light_PIN, OUTPUT);  // Set the BUZZER pin as OUTPUT 
+}  
+void loop ()  
+{
+  int Sensor_Val = digitalRead(SENSOR_PIN);  //get reading from microphone
+  
+  if (Sensor_Val == HIGH) //If Sound Sensor Detected the Clap Sound
+  {
+    digitalWrite(Light_PIN, HIGH);
+    delay(30);
+    digitalWrite(Light_PIN, LOW);
+  }
+  else
+  {
+    digitalWrite(Light_PIN, LOW);
+  }
+}
---
+++ MUSIC_REACTIVE/MUSIC_REACTIVE.ino
@@ -0,0 +1,26 @@
+int soundsensor = 8; 
+int ledstrip = 9;                 
+void setup()
+{ 
+  pinMode (soundsensor, INPUT);
+  pinMode (ledstrip, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int sensorvalue = digitalRead (soundsensor);             
+                                                          
+  if (sensorvalue == 1)                                    
+  {
+    digitalWrite(ledstrip, HIGH);
+    delay(10);
+  }
+  
+  else
+  {
+    digitalWrite(ledstrip, LOW);
+  }
+  Serial.println(sensorvalue);
+  
+}
